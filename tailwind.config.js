2b36c62265e0361662c6659ca7144b9ec7c518e7
Xavier <xavierjamito@gmail.com>
2024-04-06 18:46:30 +0800

    feat: install and setup dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
